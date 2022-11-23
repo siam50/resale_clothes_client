e49015b9e537bf58906ad228401764a83aa31c61
siam50 <siamurrahman50@gmail.com>
2022-11-23 21:35:03 +0600

    route setup completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import router from './Routes/Routes/Routes';
 
 function App() {
   return (
-    <div className="App">
-      <button className="btn btn-info">Info</button>
-      <button className="btn btn-success">Success</button>
-      <button className="btn btn-warning">Warning</button>
-      <button className="btn btn-error">Error</button>
+    <div>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
