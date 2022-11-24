4eb360ead11f1cc88840da93dccd41e3b9a7e6ff
siam50 <siamurrahman50@gmail.com>
2022-11-24 12:30:23 +0600

    login and sign up page created

--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -2,17 +2,22 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 
-const Login = () => {
+const Signup = () => {
     const { register, formState: { errors }, handleSubmit } = useForm();
 
-    const handleLogin = data => {
+    const handleSignup = data => {
         console.log(data)
     }
     return (
         <div className='h-[800px] flex justify-center items-center'>
             <div className='w-96 p-7'>
-                <h2 className='text-4xl text-center'>Login</h2>
-                <form onSubmit={handleSubmit(handleLogin)}>
+                <h2 className='text-4xl text-center'>Sign Up</h2>
+                <form onSubmit={handleSubmit(handleSignup)}>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"><span className="label-text">Full name</span></label>
+                        <input type="text" {...register("name", { required: "Name is required" })} className="input input-bordered w-full max-w-xs" />
+                        {errors.name && <p className='text-red-600'>{errors.name?.message}</p>}
+                    </div>
                     <div className="form-control w-full max-w-xs">
                         <label className="label"><span className="label-text">Email</span></label>
                         <input type="email" {...register("email", { required: "Email Address is required" })} className="input input-bordered w-full max-w-xs" />
@@ -22,15 +27,22 @@ const Login = () => {
                         <label className="label"><span className="label-text">Password</span></label>
                         <input type="password" {...register("password", { required: "Password is Required", minLength: { value: 6, message: "Password should be 6 or more" } })} className="input input-bordered w-full max-w-xs" />
                         {errors.password && <p className='text-red-600'>{errors.password?.message}</p>}
-
-                        <label className="label"><span className="label-text">Forgot Password ?</span></label>
+                        {/* <label className="label"><span className="label-text">Forgot Password ?</span></label> */}
                     </div>
-                    <input className='btn btn-accent w-full' value="Login" type="submit" />
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"><span className="label-text">As a</span></label>
+                        <select {...register("role")} className="select select-bordered w-full max-w-xs">
+                            <option >buyer</option>
+                            <option>seller</option>
+                        </select>
+                        <label className="label"></label>
+                    </div>
+                    <input className='btn btn-accent w-full' value="Sign up" type="submit" />
                     <div>
                         {/* {loginError && <p className='text-red-600'>{loginError}</p>} */}
                     </div>
                 </form>
-                <p>New to Resale Clothes? <Link className='text-secondary' to='/signup'>Create new account</Link></p>
+                <p>Have an account? <Link className='text-secondary' to='/login'>Login</Link></p>
                 <div className="divider">OR</div>
                 <button className='btn btn-outline w-full'>CONTINUE WITH GOOGLE</button>
             </div>
@@ -38,4 +50,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default Signup;
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -3,6 +3,7 @@ import Main from "../../Layout/Main";
 import Home from "../../Pages/Home/Home/Home";
 import Login from "../../Pages/Login/Login";
 import Products from "../../Pages/Products/Products";
+import Signup from "../../Pages/Signup/Signup";
 
 const router = createBrowserRouter([
     {
@@ -16,6 +17,10 @@ const router = createBrowserRouter([
             {
                 path: '/login',
                 element: <Login></Login>
+            },
+            {
+                path: '/signup',
+                element: <Signup></Signup>
             },
             {
                 path: '/category/:id',
