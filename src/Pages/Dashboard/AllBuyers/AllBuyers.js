46fa7c564af4000b26a5afccd984bf47346e65eb
siam50 <siamurrahman50@gmail.com>
2022-11-26 00:09:57 +0600

    admin role emplement

--- src/Pages/Dashboard/AllBuyers/AllBuyers.js
+++ src/Pages/Dashboard/AllBuyers/AllBuyers.js
@@ -1,9 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
 
 const AllBuyers = () => {
+
+    const [buyers, setBuyers] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/users/buyers')
+            .then(res => res.json())
+            .then(data => {
+                setBuyers(data)
+            })
+    }, [buyers]);
+
+    const handleDelete = buyer => {
+        fetch(`http://localhost:5000/users/${buyer._id}`, {
+            method: 'DELETE',
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.deletedCount > 0) {
+                    toast.success(`deleted ${buyer.name} successfully`);
+                }
+
+            })
+    }
+
     return (
         <div>
-            <h3>All Buyers Page</h3>
+            <h3 className='text-3xl font-semibold text-center'>All Buyers</h3>
+            <div className="overflow-x-auto my-7">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>User Name</th>
+                            <th>email</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            buyers.map((buyer, i) => <tr key={buyer._id}>
+                                <th>{i + 1}</th>
+                                <td>{buyer.name}</td>
+                                <td>{buyer.email}</td>
+                                <td><button onClick={() => handleDelete(buyer)} className='btn btn-sm btn-error'>delete</button></td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
--- src/Pages/Dashboard/AllSellers/AllSellers.js
+++ src/Pages/Dashboard/AllSellers/AllSellers.js
@@ -1,9 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
 
 const AllSellers = () => {
+    const [sellers, setSellers] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/users/sellers')
+            .then(res => res.json())
+            .then(data => {
+                setSellers(data)
+            })
+    }, [sellers]);
+
+    const handleDelete = seller => {
+        fetch(`http://localhost:5000/users/${seller._id}`, {
+            method: 'DELETE',
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.deletedCount > 0) {
+                    toast.success(`deleted ${seller.name} successfully`);
+                }
+
+            })
+    }
     return (
         <div>
-            <h3>All Sellers pages</h3>
+            <h3 className='text-3xl font-semibold text-center'>All Sellers</h3>
+            <div className="overflow-x-auto my-7">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>User Name</th>
+                            <th>email</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            sellers.map((seller, i) => <tr key={seller._id}>
+                                <th>{i + 1}</th>
+                                <td>{seller.name}</td>
+                                <td>{seller.email}</td>
+                                <td><button onClick={() => handleDelete(seller)} className='btn btn-sm btn-error'>delete</button></td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
